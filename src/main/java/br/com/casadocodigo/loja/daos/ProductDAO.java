a2615f0cee129d494e908e54987f6057478004d3
gilsondev <me@gilsondev.in>
2016-09-23 16:33:22 -0300

    Removendo comentarios no EntityManager do ProductDAO

--- src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProductDAO.java
@@ -3,13 +3,16 @@ package br.com.casadocodigo.loja.daos;
 import br.com.casadocodigo.loja.models.Product;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class ProductDAO {
 
-    // @PersistenceContext
-    // private EntityManager manager;
+    @PersistenceContext
+    private EntityManager manager;
 
     public void save(Product product) {
-        // manager.persist(product);
+        manager.persist(product);
     }
 }
