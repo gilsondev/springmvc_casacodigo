0489a6f6750601e08bb47b137c97af5d199bbbc0
gilsondev <me@gilsondev.in>
2016-09-23 15:32:14 -0300

    Inserindo o arquivo hello-world.jsp

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public void index() {
-        System.out.println("Carregando os produtos");
+    @RequestMapping("/")
+    public String index() {
+        return "hello-world";
     }
 }
