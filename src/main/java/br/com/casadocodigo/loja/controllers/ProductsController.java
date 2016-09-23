3c0ded79656809f1f205a69b92803c7bba0b2b88
gilsondev <me@gilsondev.in>
2016-09-23 18:11:47 -0300

    Disponibilizando os tipos de livros no cadastro de produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -1,11 +1,13 @@
 package br.com.casadocodigo.loja.controllers;
 
 import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.BookType;
 import br.com.casadocodigo.loja.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Classe que faz o papel de controller
@@ -21,8 +23,10 @@ public class ProductsController {
     private ProductDAO productDAO;
 
     @RequestMapping("/produtos/form")
-    public String form() {
-        return "products/form";
+    public ModelAndView form() {
+        ModelAndView modelAndView = new ModelAndView("products/form");
+        modelAndView.addObject("types", BookType.values());
+        return modelAndView;
     }
 
     @RequestMapping("/produtos")
--- src/main/webapp/WEB-INF/views/products/form.jsp
+++ src/main/webapp/WEB-INF/views/products/form.jsp
@@ -1,3 +1,4 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,6 +19,13 @@
         <label for="pages">Número de paginas</label>
         <input type="text" name="pages"	id="pages"/>
     </div>
+    <c:forEach items="${types}" var="bookType" varStatus="status">
+        <div>
+            <label for="price_${bookType}">${bookType}</label>
+            <input type="text" name="prices[${status.index}]" id="price_${bookType}">
+            <input type="hidden" name="prices[${status.index}].bookType" value="${bookType}">
+        </div>
+    </c:forEach>
     <div>
         <input type="submit" value="Enviar">
     </div>
